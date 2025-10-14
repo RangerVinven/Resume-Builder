b8812753503681d7b50ff2c561dd6b7eab9aad8c
RangerVinven <daniel.mcpherson@live.co.uk>
2025-10-14 14:10:26 +0100

    Initial commit

---
+++ src/main/java/dev/danielmcpherson/resume-builder/App.java
@@ -0,0 +1,10 @@
+package dev.danielmcpherson.resume-builder;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
