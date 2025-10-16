a8ef54ec2ae59648f68e1f49af254bd8982f5720
Daniel McPherson <daniel.mcpherson@live.co.uk>
2025-10-16 16:01:20 +0100

    Added use of the YAML generator

--- src/main/java/dev/danielmcpherson/resume_builder/App.java
+++ src/main/java/dev/danielmcpherson/resume_builder/App.java
@@ -5,19 +5,33 @@ import dev.danielmcpherson.resume_builder.model.*;
 
 import freemarker.template.Configuration;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class App {
     public static void main(String[] args) {
-        Resume resume = FileReader.readFile("resume.yaml");
+        // Resume resume = FileReader.readFile("resume.yaml");
+        String jobDescription = "";
+        
+        try {
+            System.out.println("Reading job description...");
+            jobDescription = new String(Files.readString(Paths.get("job_description.txt")));
+        } catch (Exception e) {
+            System.out.println("Couldn't read job description. Received error:");
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-        YAMLGenerator yamlGenerator = new YAMLGenerator("Hello, can you understand me?", Paths.get("master.txt"));
-        yamlGenerator.generateYaml();
+        YAMLGenerator yamlGenerator = new YAMLGenerator(jobDescription, Paths.get("master.txt"));
+        Resume resume = FileReader.readFromString(yamlGenerator.generateYaml());
+        // yamlGenerator.saveYaml(resume);
 
         try {
+            System.out.println("Building resume...");
             Configuration configuration = FreemarkerUtil.createConfiguration();
             String html = FreemarkerUtil.renderResumeHtml(configuration, resume);
             FreemarkerUtil.renderToPDF(configuration, html, Paths.get("new_resume.pdf"));
 
+            System.out.println("Resume built!");
         } catch (Exception e) {
             System.out.println("Couldn't create PDF. Recieved error: " + e.toString());
             System.exit(1);
