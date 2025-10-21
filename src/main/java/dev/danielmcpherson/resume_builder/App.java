2894376687ffc9a5fe895ac5bf62384bc9c07cd5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2025-10-21 12:39:29 +0100

    Added arguments when running the program

--- src/main/java/dev/danielmcpherson/resume_builder/App.java
+++ src/main/java/dev/danielmcpherson/resume_builder/App.java
@@ -4,39 +4,83 @@ import dev.danielmcpherson.resume_builder.util.*;
 import dev.danielmcpherson.resume_builder.model.*;
 
 import freemarker.template.Configuration;
+
+import java.nio.file.StandardOpenOption;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.nio.file.Files;
 
 public class App {
     public static void main(String[] args) {
-        // String jobDescription = "";
-        //
-        // try {
-        //     System.out.println("Reading job description...");
-        //     jobDescription = new String(Files.readString(Paths.get("job_description.txt")));
-        // } catch (Exception e) {
-        //     System.out.println("Couldn't read job description. Received error:");
-        //     e.printStackTrace();
-        //     System.exit(1);
-        // }
+        if(args.length == 0 || (!args[0].equalsIgnoreCase("build") && !args[0].equalsIgnoreCase("generate") && !args[0].equalsIgnoreCase("help"))) {
+            System.out.println("First argument must be either 'build' (for building the resume based of a given yaml file) or 'generate' (for generating the resume based off a job description and master file).");
+            System.exit(0);
+        }
 
-        // YAMLGenerator yamlGenerator = new YAMLGenerator(jobDescription, Paths.get("master.txt"));
+        if(args[0].equalsIgnoreCase("build")) {
+            buildResume(args);
+        }
 
-        // Resume resume = FileReader.readFromString(generatedYAML);
-        
-        // String generatedYAML = yamlGenerator.generateResume();
-        Resume resume = FileReader.readFile("resume.yaml");
+        if(args[0].equalsIgnoreCase("generate")) {
+            generateResume(args);
+        }
+    }
 
-        System.out.println("Saving the resume...");
+    private static void buildResume(String[] args) {
+        if(args.length != 3) {
+            System.out.println("Must be in the format of 'resume-builder build <inputYamlFile> <outputFile>'.");
+            System.exit(0);
+        }
 
+        String resumeYamlFile = args[1];
+        String outputFile = args[2];
+
+        Resume resume = FileReader.readFile(resumeYamlFile);
+        writeResumeToPDF(resume, outputFile);
+    }
+
+    private static void generateResume(String[] args) {
+        if(args.length != 4) {
+            System.out.println("Must be in the format of 'resume-builder generate <job_description> <master_file> <output_folder>'.");
+            System.exit(0);
+        }
+
+        String jobDescriptionFile = args[1];
+        String masterFile = args[2];
+        Path outputFolder = Path.of(args[3]);
+
+        YAMLGenerator yamlGenerator = new YAMLGenerator(jobDescriptionFile, masterFile);
+        String generatedResume = yamlGenerator.generateResume();
+
+        Resume resume = FileReader.readFromString(generatedResume);
+
+        try {
+            Files.createDirectories(outputFolder);
+            String baseName = outputFolder.getFileName().toString();
+
+            Files.write(
+                outputFolder.resolve(baseName + ".yaml"),
+                generatedResume.getBytes(),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING
+            );
+
+            writeResumeToPDF(resume, outputFolder.resolve(baseName + ".pdf").toString());
+
+        } catch (Exception e) {
+            System.out.println("Failed to generate resume. Recieved error: ");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void writeResumeToPDF(Resume resume, String outputFile) {
         try {
             System.out.println("Building resume...");
             Configuration configuration = FreemarkerUtil.createConfiguration();
             String html = FreemarkerUtil.renderResumeHtml(configuration, resume);
 
-            // String fileName = YAMLGenerator.saveYaml(generatedYAML, resume);
-            String fileName = Long.toString(System.currentTimeMillis());
-            FreemarkerUtil.renderToPDF(configuration, html, Paths.get("new_resume.pdf"));
+            FreemarkerUtil.renderToPDF(configuration, html, Paths.get(outputFile));
 
             System.out.println("Resume built and saved!");
         } catch (Exception e) {
@@ -44,4 +88,4 @@ public class App {
             System.exit(1);
         }
     }
-}
+} 
