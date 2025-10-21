aae7b0409b13d58b2229a902548b3427bbfb7376
Daniel McPherson <daniel.mcpherson@live.co.uk>
2025-10-21 14:54:39 +0100

    Added show_bullets meta_subtitle attributes

--- src/main/java/dev/danielmcpherson/resume_builder/model/SectionItem.java
+++ src/main/java/dev/danielmcpherson/resume_builder/model/SectionItem.java
@@ -4,17 +4,28 @@ import java.util.List;
 
 public class SectionItem {
     private String title;
+    private Boolean show_bullets = true;
     private String subtitle;
     private String meta;
+    private String meta_subtitle;
     private List<String> bullet_points;
 
     public SectionItem() {}
 
-    public SectionItem(String title, String subtitle, String meta, List<String> bullet_points) {
+    public SectionItem(String title, String subtitle, String meta, List<String> bullet_points, String meta_subtitle) {
         this.title = title;
         this.subtitle = subtitle;
         this.meta = meta;
         this.bullet_points = bullet_points;
+        this.meta_subtitle = meta_subtitle;
+    }
+
+    public SectionItem(String title, String subtitle, String meta, List<String> bullet_points, Boolean show_bullets) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.meta = meta;
+        this.bullet_points = bullet_points;
+        this.show_bullets = show_bullets;
     }
 
     public String getTitle() {
@@ -47,6 +58,22 @@ public class SectionItem {
 
     public void setBullet_points(List<String> bullet_points) {
         this.bullet_points = bullet_points;
+    }
+
+    public Boolean getShow_bullets() {
+        return show_bullets;
+    }
+
+    public void setShow_bullets(Boolean show_bullets) {
+        this.show_bullets = show_bullets;
+    }
+
+    public String getMeta_subtitle() {
+        return meta_subtitle;
+    }
+
+    public void setMeta_subtitle(String meta_subtitle) {
+        this.meta_subtitle = meta_subtitle;
     }
 }
 
